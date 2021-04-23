942ed45fa9ec12f7fd823620d3fe2cdcc4d39990
amar-ak <amarnath.karwekar@gmail.com>
2021-04-22 18:47:46 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
-import { componentFactoryResolverProviderDef } from "@angular/compiler/src/view_compiler/provider_compiler";
 import { Component, Input, VERSION } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { APIDetail } from "./APIDetail";
-
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "CPQ API Analyzer " + "0.1";
+  name = "CPQ API Analyzer " + "1.0";
   userSelectionOnlyAPI: boolean = false;
   userSelectionGrouped: boolean = false;
   userSelectionRawAPI: boolean = true;
-
+  fileNameToUse: string = "report";
   fileToUpload: File = null;
   handleOnlyAPIChange = evt => {
     this.userSelectionOnlyAPI = evt.target.checked;
@@ -34,10 +32,8 @@ export class AppComponent {
     //reset
     evt.target.value = null;
   };
-
   handleFileInput = event => {
     var self = this;
-
     function getLevel() {
       let lvl;
       if (self.userSelectionGrouped) lvl = "Fine";
@@ -45,15 +41,19 @@ export class AppComponent {
       else if (self.userSelectionRawAPI === true) lvl = "Raw";
       return lvl;
     }
-
-    // eval("remotingcall('Download',getLevel(),'HARParser')");
+    function getFileNamePrefix() {
+      let lvl;
+      if (self.userSelectionGrouped) lvl = "groupedaction";
+      else if (self.userSelectionOnlyAPI) lvl = "groupedapi";
+      else if (self.userSelectionRawAPI === true) lvl = "rawapi";
+      return lvl;
+    }
     let fileList: FileList = event.target.files;
     let apiList: String[] = ["updatePrice", "updateCartLineItems"];
     if (fileList.length > 0) {
       let file: File = fileList[0];
-
+      self.fileNameToUse = file.name;
       let reader = new FileReader();
-
       reader.onload = e => {
         var self = this;
         let csvToUse;
@@ -61,16 +61,15 @@ export class AppComponent {
         let nonParsedApis = new Map();
         let rawApis: APIDetail[] = [];
         let allData = new Map();
+        let allOtherTime = 0;
+        let allOtherCallCount = 0;
         let superMap: [Map<string, APIDetail>, Map<string, APIDetail>];
-
         var jsonDt = JSON.parse(e.target.result.toString());
         // console.log(jsonDt) ;
         var jsonObj = jsonDt.log.entries;
         //console.log(jsonObj) ;
-
         let flatten = function(data) {
           var result = {};
-
           function addDataWithinMaps(theMap, key, dataTowork, action) {
             if (theMap.get(key) == null) {
               if (dataTowork.time != null) {
@@ -95,7 +94,6 @@ export class AppComponent {
                 recTime.WaitTime = dataTowork.timings.wait;
                 recTime.RecieveTime = dataTowork.timings.receive;
                 recTime.MarkerAction = action;
-
                 theMap.set(key, recTime);
               }
             }
@@ -112,12 +110,10 @@ export class AppComponent {
               switch (parseStrategy) {
                 case "Coarse": {
                   addDataWithinMaps(parsedApis, cpqMethod, req, action);
-
                   break;
                 }
                 case "Fine": {
                   addDataWithinMaps(parsedApis, category, req, action);
-
                   break;
                 }
                 case "Raw": {
@@ -132,7 +128,6 @@ export class AppComponent {
                   data.MarkerAction = action;
                   setter.push(data);
                   //addDataWithinMaps(rawApis,cpqMethod,req) ;
-
                   break;
                 }
                 default: {
@@ -154,7 +149,6 @@ export class AppComponent {
             function addAction() {
               return "Action" + i++;
             }
-
             function initializeMaps() {
               parsedApis = new Map();
               nonParsedApis = new Map();
@@ -166,21 +160,11 @@ export class AppComponent {
             superMap = [parsedApis, nonParsedApis];
             var action = addAction();
 
-            //for raw APIs
-            /* let apiDtl = new APIDetail();
-            apiDtl.MarkerAction = action;
-            apiDtl.APIName = "";
-            apiDtl.NetTime = null;
-            apiDtl.NumberOfCalls = null;
-
-            rawApis.push(apiDtl);*/
-
             allData.set(action, superMap);
-
             for (let entry of payLoad) {
               if (
                 entry.request.method == "POST" &&
-                entry.request.postData !== null &&
+                entry.request.postData != null &&
                 !(
                   (entry.request.postData.text != null &&
                     entry.request.postData.text.includes("long-polling")) ||
@@ -188,151 +172,165 @@ export class AppComponent {
                 )
               ) {
                 let apiInLoad = entry.request.postData.text;
-
                 let postJData;
                 try {
                   postJData = JSON.parse(apiInLoad);
                 } catch (e) {
                   continue;
                 }
-
                 if (postJData.method == "performAction") {
                   if (
                     postJData.data != null &&
                     postJData.data[0] != null &&
                     ((postJData.data[0].displayAction != null &&
                       postJData.data[0].displayAction.ActionLabelName ==
-                        "Add Marker") ||
+                        "QuickSave") ||
                       superMap.entries.length == 0)
                   ) {
                     initializeMaps();
                   }
-                }
-                //  debugger ;
-                console.log("Method :" + postJData.method);
-                // var downloadAll = document.getElementById('onlyAPI') ;
-                var downloadGrouped = document.getElementById("groupedAPI");
-                //var grouped = downloadGrouped.getAttribute('checked') ;
-
-                let refineLevel = getLevel();
-
-                /*
-                if (self.userSelectionGrouped) refineLevel = "Fine";
-                else if (self.userSelectionOnlyAPI) refineLevel = "Coarse";
-                else if (self.userSelectionRawAPI === true) refineLevel = "Raw";*/
-
-                switch (postJData.method) {
-                  case "updatePrice":
-                  case "updateCartLineItems": {
-                    addtoList(
-                      action,
-                      "Pricing",
-                      postJData.method,
-                      entry,
-                      refineLevel
-                    );
-                    break;
-                  }
-                  case "getGuidePageUrl":
-                  case "getCategories": {
-                    addtoList(
-                      action,
-                      "LaunchCatalog",
-                      postJData.method,
-                      entry,
-                      refineLevel
-                    );
-                  }
-
-                  case "addToCart": {
-                    addtoList(
-                      action,
-                      "AddToCart",
-                      postJData.method,
-                      entry,
-                      refineLevel
-                    );
-
-                    break;
-                  }
-
-                  case "searchProducts":
-                  case "getConfigurationData": {
-                    addtoList(
-                      action,
-                      "SearchProducts",
-                      postJData.method,
-                      entry,
-                      refineLevel
-                    );
-
-                    break;
-                  }
-
-                  case "getDefaultLineItemRollup":
-                  case "getCart":
-                  case "getCartLineNumbers":
-                  case "getLineItemFieldsMetaData":
-                  case "getSObjectPermissions":
-                  case "getAllCartViews":
-                  case "getObjectForSummary":
-                  case "getAnalyticsRecommendedProducts":
-                  case "getReferenceObjects":
-                  case "getCartLineItems":
-                  case "getChildCartLineItems":
-                  case "getProductDetails": {
-                    addtoList(
-                      action,
-                      "CartLaunch",
-                      postJData.method,
-                      entry,
-                      refineLevel
-                    );
-
-                    break;
-                  }
-
-                  case "getProductDetails": {
-                    addtoList(
-                      action,
-                      "CartLaunch",
-                      postJData.method,
-                      entry,
-                      refineLevel
-                    );
-
-                    break;
-                  }
-                  default: {
-                    if (postJData.method == "performAction")
+                } else {
+                  //  debugger ;
+                  console.log("Method :" + postJData.method);
+                  // var downloadAll = document.getElementById('onlyAPI') ;
+                  var downloadGrouped = document.getElementById("groupedAPI");
+                  //var grouped = downloadGrouped.getAttribute('checked') ;
+                  let refineLevel = getLevel();
+                  /*
+if (self.userSelectionGrouped) refineLevel = "Fine";
+else if (self.userSelectionOnlyAPI) refineLevel = "Coarse";
+else if (self.userSelectionRawAPI === true) refineLevel = "Raw";*/
+                  switch (postJData.method) {
+                    case "updatePrice":
+                    case "updateCartTotals":
+                    case "updateCartLineItems": {
                       addtoList(
                         action,
-                        postJData.method,
+                        "Pricing",
                         postJData.method,
                         entry,
                         refineLevel
                       );
-
-                    break;
+                      break;
+                    }
+                    case "updateLineItemSequence":
+                    case "cloneBundleLineItem": {
+                      addtoList(
+                        action,
+                        "Copy Cart Lines",
+                        postJData.method,
+                        entry,
+                        refineLevel
+                      );
+                      break;
+                    }
+                    case "getGuidePageUrl":
+                    case "getRecommendedProducts":
+                    case "getGuidePageUrl":
+                    case "getCategories": {
+                      addtoList(
+                        action,
+                        "LaunchCatalog",
+                        postJData.method,
+                        entry,
+                        refineLevel
+                      );
+                    }
+                    case "executeConstraintRules":
+                    case "configureBundle":
+                    case "getConstraintRuleProducts":
+                    case "saveConstraintsResults":
+                    case "addToCart": {
+                      addtoList(
+                        action,
+                        "AddToCart",
+                        postJData.method,
+                        entry,
+                        refineLevel
+                      );
+                      break;
+                    }
+                    case "searchFavoriteConfigurations":
+                    case "getProductGroups":
+                    case "searchProducts":
+                    case "getConfigurationData": {
+                      addtoList(
+                        action,
+                        "SearchProducts",
+                        postJData.method,
+                        entry,
+                        refineLevel
+                      );
+                      break;
+                    }
+                    case "createRollupSummaryGroupsForConfigCart":
+                    case "initCart":
+                    case "getCartSummary":
+                    case "getConfigCartDO":
+                    case "getDefaultLineItemRollup":
+                    case "getCart":
+                    case "getCartLineNumbers":
+                    case "getLineItemFieldsMetaData":
+                    case "getSObjectPermissions":
+                    case "getAllCartViews":
+                    case "getObjectForSummary":
+                    case "getAnalyticsRecommendedProducts":
+                    case "getReferenceObjects":
+                    case "getCartLineItems":
+                    case "getChildCartLineItems":
+                    case "getProductDetails": {
+                      addtoList(
+                        action,
+                        "CartLaunch",
+                        postJData.method,
+                        entry,
+                        refineLevel
+                      );
+                      break;
+                    }
+                    default: {
+                      if (refineLevel == "Fine") {
+                        addDataWithinMaps(
+                          nonParsedApis,
+                          postJData.method,
+                          entry,
+                          action
+                        );
+                      } else {
+                        addtoList(
+                          action,
+                          postJData.method,
+                          postJData.method,
+                          entry,
+                          refineLevel
+                        );
+                      }
+                      console.log("Defaulted method: " + postJData.method);
+                      console.log("Untracked data: " + JSON.stringify(entry));
+                      break;
+                    }
                   }
                 }
+              } else {
+                allOtherTime += entry.time;
+                allOtherCallCount++;
+                console.log("Misc call URL: " + entry.request.url);
+                console.log("Misc call time: " + entry.time);
               }
             }
           }
           search(data);
           return parsedApis;
         };
-
         let apiData = flatten(jsonObj);
         console.log("Tracked apiData" + JSON.stringify(apiData));
         console.log("Non grouped apiData" + JSON.stringify(nonParsedApis));
         console.log("Non grouped raw apiData" + JSON.stringify(rawApis));
-
         let downloader = function downloadAsCSV() {
           let csvData: string = "";
-
           let refineLvl = getLevel();
           console.debug(refineLvl);
+
           csvData +=
             "ActionGroup" +
             "," +
@@ -343,19 +341,15 @@ export class AppComponent {
             "Total Calls" +
             "," +
             "Avg Time Per Call";
-
           if (refineLvl == "Raw") {
             csvData += "," + "Wait Time" + "," + "Block time";
           }
           csvData += "\r\n";
-
           csvData += "" + "," + "," + "," + ",";
           if (refineLvl == "Raw") {
             csvData += "," + "," + ",";
           }
-
           csvData += "\r\n";
-
           if (self.userSelectionRawAPI === true) {
             rawApis.forEach((value: APIDetail) => {
               csvData +=
@@ -368,7 +362,6 @@ export class AppComponent {
                 (value.APIName != "" ? "1" : "") +
                 "," +
                 (value.APIName != "" ? value.NetTime : "");
-
               if (refineLvl == "Raw") {
                 csvData +=
                   "," +
@@ -376,7 +369,6 @@ export class AppComponent {
                   "," +
                   (value.APIName != "" ? value.BlockTime : "");
               }
-
               csvData += "\r\n";
             });
           } else {
@@ -408,7 +400,6 @@ export class AppComponent {
                   "," +
                   "," +
                   "\r\n";
-
                 value[1].forEach((value: APIDetail, keyInner: string) => {
                   csvData +=
                     key +
@@ -428,16 +419,28 @@ export class AppComponent {
               }
             );
           }
-
+          csvData +=
+            "Misc Calls" +
+            "," +
+            "These are usually GET requests like images|css|etc " +
+            "," +
+            allOtherTime +
+            "," +
+            allOtherCallCount +
+            "," +
+            allOtherTime / allOtherCallCount +
+            "\r\n";
           var blob = new Blob([csvData], { type: "text/csv" });
           var url = window.URL.createObjectURL(blob);
-
+          function getfileNameToUse() {
+            return self.fileNameToUse + "_" + getLevel() + ".csv";
+          }
           if (navigator.msSaveOrOpenBlob) {
-            navigator.msSaveBlob(blob, "download_report.csv");
+            navigator.msSaveBlob(blob, getfileNameToUse());
           } else {
             var a = document.createElement("a");
             a.href = url;
-            a.download = "download_report.csv";
+            a.download = getfileNameToUse();
             document.body.appendChild(a);
             a.click();
             document.body.removeChild(a);
@@ -446,8 +449,15 @@ export class AppComponent {
         };
         downloader();
       };
-
-      reader.readAsText(file);
+      try {
+        reader.readAsText(file);
+        //eval("remotingcall('Download',getLevel(),'HARParser','Success')");
+      } catch (error) {
+        alert(
+          "Problem processing HAR. Please contact support : Error -" + error
+        );
+        //eval("remotingcall('Download',getLevel(),'HARParser'," + error + ")");
+      }
     }
   };
 }
